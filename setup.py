dd368ebf2ea1d2e8d1bff9cf41f872c3b46e0dc8
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-05-24 18:09:01 +0800

    [fix] fix potential ModuleNotFoundError

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 test_deps = [
     'pytest>=4',
@@ -8,10 +8,11 @@ test_deps = [
 
 setup(
     name='EduKTM',
-    version='0.0.3',
+    version='0.0.4',
     extras_require={
         'test': test_deps,
     },
+    packages=find_packages(),
     install_requires=[
         "torch",
         "tqdm",
