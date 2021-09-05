b39f11627245090127c058ea1c56baf1a0a9b58d
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-09-05 18:36:45 +0800

    [examples] GKT

--- examples/GKT/GKT.py
+++ examples/GKT/GKT.py
@@ -4,18 +4,18 @@
 import logging
 from EduKTM.GKT import etl, extract_graph
 
-from EduKTM import GKT
+from EduKTM.GKT import GKT
 
 if __name__ == '__main__':
     batch_size = 16
-    train = etl("../../data/a0910c/train.json", batch_size)
-    valid = etl("../../data/a0910c/valid.json", batch_size)
-    test = etl("../../data/a0910c/test.json", batch_size)
-    graph = extract_graph(146)
+    train = etl("../../data/assistment_2009_2010/train.json", batch_size)
+    valid = etl("../../data/assistment_2009_2010/test.json", batch_size)
+    # test = etl("../../data/a0910c/test.json", batch_size)
+    graph = extract_graph(124)
 
     logging.getLogger().setLevel(logging.INFO)
 
-    gkt = GKT(graph, item_dim=32, state_dim=32)
+    gkt = GKT(graph, item_dim=5, state_dim=5)
     gkt.train(train, epoch=2)
 # gkt.save("gkt.params")
 
